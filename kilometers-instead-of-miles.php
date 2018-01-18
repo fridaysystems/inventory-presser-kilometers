e820a6970a3dc569c738276d8f6087bb24b5c2d5
Corey Salzano <email@coreysalzano.com>
2018-01-18 11:18:23 -0500

    Make this plugin translation-ready

--- kilometers-instead-of-miles.php
+++ kilometers-instead-of-miles.php
@@ -6,6 +6,8 @@
  * Version: 1.1.0
  * Author: Corey Salzano
  * Author URI: https://profiles.wordpress.org/salzano
+ * Text Domain: inventory-presser-kilometers
+ * Domain Path: /languages
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
@@ -13,6 +15,11 @@
 class Kilometers_Instead_of_Miles {
 
 	function hooks() {
+		//Allow translations
+		add_action( 'plugins_loaded', function() {
+			load_plugin_textdomain( 'inventory-presser-kilometers', false, __DIR__ );
+		} );
+
 		add_filter( 'invp_odometer_word', array( &$this, 'replace_miles_with_kilometers' ) );
 		add_filter( '_dealer_odometer_word', array( &$this, 'replace_miles_with_kilometers' ) );
 	}
@@ -20,13 +27,13 @@ class Kilometers_Instead_of_Miles {
 	function replace_miles_with_kilometers( $word ) {
 		switch( $word ) {
 			case 'Mileage':
-				return 'Kilometrage';
+				return __( 'Kilometrage', 'inventory-presser-kilometers' );
 			case 'mileage':
-				return 'kilometrage';
+				return __( 'kilometrage', 'inventory-presser-kilometers' );
 			case 'Miles':
-				return 'Kilometers';
+				return __( 'Kilometers', 'inventory-presser-kilometers' );
 			case 'miles':
-				return 'kilometers';
+				return __( 'kilometers', 'inventory-presser-kilometers' );
 		}
 		return $word;
 	}
