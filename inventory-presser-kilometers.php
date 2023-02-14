ea01a3a89abbb2ab3092bb95dc40c2ca6ffb653c
Corey Salzano <corey.salzano@gmail.com>
2023-02-13 21:47:21 -0500

    Applies WP coding standard, adds early exit to the file. Updates plugin header comment. Renames hooks() to add_hooks().

--- inventory-presser-kilometers.php
+++ inventory-presser-kilometers.php
@@ -1,28 +1,41 @@
 <?php
+defined( 'ABSPATH' ) || exit;
+
 /**
- * Plugin Name: Inventory Presser Kilometers Instead of Miles
- * Plugin URI: https://github.com/fridaysystems/inventory-presser-kilometers
+ * Plugin Name: Inventory Presser - Kilometers Instead of Miles
+ * Plugin URI: https://inventorypresser.com/downloads/kilometers-instead-of-miles/
  * Description: Add-on for Inventory Presser. Changes odometer units to kilometers.
  * Version: 1.2.0
  * Author: Corey Salzano
- * Author URI: https://profiles.wordpress.org/salzano
+ * Author URI: https://github.com/fridaysystems/inventory-presser-kilometers
  * Text Domain: inventory-presser-kilometers
  * Domain Path: /languages
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
 
+/**
+ * Kilometers_Instead_of_Miles
+ */
 class Kilometers_Instead_of_Miles {
 
-	function hooks() {
-		//Allow translations
-		add_action( 'plugins_loaded', function() {
-			load_plugin_textdomain( 'inventory-presser-kilometers', false, __DIR__ );
-		} );
+	/**
+	 * hooks
+	 *
+	 * @return void
+	 */
+	public function add_hooks() {
+		// Allow translations.
+		add_action(
+			'plugins_loaded',
+			function() {
+				load_plugin_textdomain( 'inventory-presser-kilometers', false, __DIR__ );
+			}
+		);
 
-		//For units in the core plugin
+		// For units in the core plugin.
 		add_filter( 'invp_odometer_word', array( $this, 'replace_miles_with_kilometers' ) );
-		//For units in the _dealer theme
+		// For units in the _dealer theme.
 		add_filter( '_dealer_odometer_word', array( $this, 'replace_miles_with_kilometers' ) );
 	}
 
@@ -41,4 +54,4 @@ class Kilometers_Instead_of_Miles {
 	}
 }
 $kilometers2384907234 = new Kilometers_Instead_of_Miles();
-$kilometers2384907234->hooks();
+$kilometers2384907234->add_hooks();
